9e37d2dd7ac0b15799347d0770d372f4955296e0
emmy-na <imanenahmad@gmail.com>
2025-10-17 17:23:33 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3100',  
+        changeOrigin: true,
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+    },
+  },
+})
